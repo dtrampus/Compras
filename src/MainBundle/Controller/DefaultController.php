0a48c30bf42e3689c86802a6ea3acf0906fc7b28
usuario <usuario@ubuntu>
2015-10-27 13:35:25 -0300

    Proyecto compras

---
+++ src/MainBundle/Controller/DefaultController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace MainBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+//        $connectionFactory = $this->container->get('doctrine.dbal.connection_factory');
+//        $connection = $connectionFactory->createConnection(array(
+//            'driver' => 'pdo_mysql',
+//            'user' => 'root',
+//            'password' => '123456',
+//            'host' => 'localhost',
+//            'dbname' => 'db_sucursal1',
+//        ));
+//       
+//       
+//       $stmt = $connection->prepare("select * from db_principal.blmercaderia, blcompras;");
+//       $stmt->execute();
+//       $rResultFilteres = $stmt->fetch();
+       
+       
+        return $this->render('MainBundle:Default:index.html.twig');
+    }
+    
+    public function sucursalesAction(){
+        $em = $this->getDoctrine()->getManager();
+        $sucursales = $em->getRepository('MainBundle:Sucursal')->findAll();
+       
+        return $this->render('MainBundle:Default:sucursales.html.twig', array(
+                    'sucursales' => $sucursales
+        ));
+    }
+}
---
+++ src/UserBundle/Entity/User.php
@@ -0,0 +1,134 @@
+<?php
+// src/UserBundle/Entity/User.php
+
+namespace UserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="users")
+ * @UniqueEntity("username")
+ * @UniqueEntity("email")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    
+    /**
+     * @ORM\Column(type="string", length=100, nullable=true)
+     */
+    private $firstname;
+    
+    /**
+     * @ORM\Column(type="string", length=100, nullable=true)
+     */
+    private $lastname;
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=true, name="nombre_bd")
+     */
+    private $nombreBd;
+    
+    /**
+     * @ORM\ManyToMany(targetEntity="UserBundle\Entity\Group")
+     * @ORM\JoinTable(name="users_groups",
+     *      joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="group_id", referencedColumnName="id")}
+     * )
+     */
+    protected $groups;
+    
+    public function __construct()
+    {
+        parent::__construct();
+        $this->groups = new ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set firstname
+     *
+     * @param string $firstname
+     * @return User
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    /**
+     * Get firstname
+     *
+     * @return string 
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * Set lastname
+     *
+     * @param string $lastname
+     * @return User
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    /**
+     * Get lastname
+     *
+     * @return string 
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+    
+    /**
+     * Set nombreBd
+     *
+     * @param string $nombreBd
+     * @return User
+     */
+    public function setNombreBd($nombreBd)
+    {
+        $this->nombreBd = $nombreBd;
+
+        return $this;
+    }
+
+    /**
+     * Get nombreBd
+     *
+     * @return string 
+     */
+    public function getNombreBd()
+    {
+        return $this->nombreBd;
+    }
+}
