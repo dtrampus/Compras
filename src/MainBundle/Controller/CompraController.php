9d55ffd241091645d8d61f083c058efa1f0de318
usuario <usuario@Skynet>
2015-11-06 10:31:25 -0300

    Listado de Compras + Pantalla de Edicion

--- src/MainBundle/Controller/CompraController.php
+++ src/MainBundle/Controller/CompraController.php
@@ -17,7 +17,7 @@ class CompraController extends Controller
      * Lists all Sucursal entities.
      *
      */
-    public function indexAction()
+    public function nuevaCompraAction()
     {
         $proveedores = $this->getDoctrine()->getManager("dinamica")->getConnection()->prepare("select id, nombre from db_principal.blproveedores p");
         $proveedores->execute();
@@ -27,7 +27,7 @@ class CompraController extends Controller
         $rubros->execute();
         $filasRubros = $rubros->fetchAll();
         
-        return $this->render('MainBundle:Compra:index.html.twig',array(
+        return $this->render('MainBundle:Compra:nueva.html.twig',array(
             "proveedores" => $filasProveedores,
             "rubros" => $filasRubros
         ));
@@ -49,6 +49,56 @@ class CompraController extends Controller
             $output[] = $fila;
         }
         unset($listaCompras);
+        return new JsonResponse($output);
+    }
+    
+    public function listarComprasAction()
+    {
+        $compras = $this->getDoctrine()->getManager("dinamica")->getConnection()->prepare("SELECT c.id, c.fecha, p.nombre, c.total FROM db_sucursal1.blcompras c INNER JOIN db_principal.blproveedores p ON p.id = c.blproveedores_id");
+        $compras->execute();
+        $filasCompras = $compras->fetchAll();
+        
+        return $this->render('MainBundle:Compra:listar.html.twig',array(
+            "compras" => $filasCompras
+        ));
+    }
+    
+        public function editarAction($id)
+    {
+        $proveedores = $this->getDoctrine()->getManager("dinamica")->getConnection()->prepare("select id, nombre from db_principal.blproveedores p");
+        $proveedores->execute();
+        $filasProveedores = $proveedores->fetchAll();
+        
+        $rubros = $this->getDoctrine()->getManager("dinamica")->getConnection()->prepare("select id, descripcion from db_principal.blrubros r");
+        $rubros->execute();
+        $filasRubros = $rubros->fetchAll();
+        
+//        $compra = $this->getDoctrine()->getManager("dinamica")->getConnection()->prepare("SELECT id FROM db_sucursal1.blcompras c WHERE id = $id");
+//        $compra->execute();
+//        $filasCompra = $compra->fetch();
+        
+        return $this->render('MainBundle:Compra:editar.html.twig',array(
+            "proveedores" => $filasProveedores,
+            "rubros" => $filasRubros,
+            "id" => $id
+        ));
+    }
+    
+        public function listarEditarAction(Request $request)
+    {
+        $compraId = intval($request->request->get("compraId")); 
+            
+        $listarEditarCompras = $this->get('main_compra_repositorio')->listarEditarMercaderia($compraId);
+        
+        $output = array();
+        foreach ($listarEditarCompras as $aRow) {
+            $fila = array();
+            foreach ($aRow as $valor) {
+                $fila[] = $valor;
+            }
+            $output[] = $fila;
+        }
+        unset($listarEditarCompras);
         return new JsonResponse($output);
     }
 }
--- src/MainBundle/Entity/CompraRepositorio.php
+++ src/MainBundle/Entity/CompraRepositorio.php
@@ -1,14 +1,16 @@
 <?php
+
 namespace MainBundle\Entity;
 
-class CompraRepositorio{
-    
+class CompraRepositorio {
+
     private $doctrine;
-    public function __construct($doctrine){
+
+    public function __construct($doctrine) {
         $this->doctrine = $doctrine;
     }
-    
-    public function listarMercaderia($fecha,$rubroId,$proveedorId){
+
+    public function listarMercaderia($fecha, $rubroId, $proveedorId) {
         $query = "
             SELECT cd.cantidad, CONCAT(m.codigo,' - ',m.descripcion,' - ',s.nombre) AS insumo, r.descripcion AS rubro, cd.importe/IFNULL(cd.cantidad,0) AS precio_unitario, cd.importe AS total
             FROM db_principal.blmercaderia m
@@ -31,5 +33,27 @@ class CompraRepositorio{
         $resultado = $consulta->fetchAll();
         return $resultado;
     }
-      
+
+    public function listarEditarMercaderia($compraId) {
+        $query = "
+            SELECT ROUND(cd.cantidad,0) AS cantidad, CONCAT(m.codigo,' - ',m.descripcion,' - ',s.nombre) AS insumo, r.descripcion AS rubro, ROUND(cd.importe/IFNULL(cd.cantidad,0),2) AS precio_unitario, cd.importe AS total
+            FROM db_principal.blmercaderia m
+                INNER JOIN db_principal.sis_unidad_medida s 
+                    ON m.sis_unidad_medida_id = s.id
+                INNER JOIN db_principal.blrubros r
+                    ON m.blrubros_id = r.id
+                LEFT JOIN blcompras_detalle cd
+                    ON m.id = cd.blmercaderia_id
+                LEFT JOIN blcompras c
+                    ON cd.blcompras_id = c.id
+                LEFT JOIN db_principal.blproveedores p
+                    ON c.blproveedores_id = p.id
+            WHERE IFNULL(c.id, $compraId) = $compraId
+        ";
+        $consulta = $this->doctrine->getEntityManager("dinamica")->getConnection()->prepare($query);
+        $consulta->execute();
+        $resultado = $consulta->fetchAll();
+        return $resultado;
+    }
+
 }
